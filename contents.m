1a8fd46b78caf792b9c0c8db01c5479f282b4929
elboyran <e.ranguelova@esciencecenter.nl>
2016-07-15 16:30:31 +0200

    Detectors used only for research are moved into the LargeScaleImaging repo

--- contents.m
+++ contents.m
@@ -2,12 +2,10 @@
 %
 % topic: Salient regions detection
 % author: Elena Ranguelova, NLeSc
-% date: September 2015
+% date: July 2016
 %
 %**************************************************************************
 % DMSR directory- contains the code for the DMSR detector
-% SMSSR directory- contains the code for the SMSSR detector
 % MSSR directory- contains the code for the MSSR detector
-% MSER directory- contains the code for the MSER detector
-% Testing directory- contains the scripts for repeatability testing
+% Testing directory- contains the scripts for testing
 %**************************************************************************
